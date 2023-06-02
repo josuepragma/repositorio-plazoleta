2ad585a7118083c4bd5f8a7c48b02f3ac4f788af
josuepragma <josue.gutierrez@pragma.com.co>
2023-06-02 11:15:15 -0500

    creating small square microservice

---
+++ src/main/java/com/pragma/smallsquare/SmallSquareMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.smallsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSquareMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallSquareMicroserviceApplication.class, args);
+	}
+
+}
