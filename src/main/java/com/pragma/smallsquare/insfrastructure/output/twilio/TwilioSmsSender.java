39a9b425f5c092c136e54083d5be5c9561d5bcdf
josuepragma <josue.gutierrez@pragma.com.co>
2023-06-28 15:02:21 -0500

    Corrigiendo errores twilio SonarCloud v1

--- src/main/java/com/pragma/smallsquare/insfrastructure/output/twilio/TwilioSmsSender.java
+++ src/main/java/com/pragma/smallsquare/insfrastructure/output/twilio/TwilioSmsSender.java
@@ -3,14 +3,18 @@ package com.pragma.smallsquare.insfrastructure.output.twilio;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TwilioSmsSender {
 
-    public static final String ACCOUNT_SID = "AC3047a197f006fc8c80c61b4fb6493ed3";
-    public static final String AUTH_TOKEN = "5837bb5a9a6c04d74df2dd639719c8a3";
-    public static final String PHONE_NUMBER = "+14178043060";
+    @Value("${twilio.account-sid}")
+    public static String ACCOUNT_SID;
+    @Value("${twilio.auth-token}")
+    public static String AUTH_TOKEN;
+    @Value("${twilio.phone-number}")
+    public static String PHONE_NUMBER;
 
     public void sendSms(String toPhoneNumber, String message) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
